36d80b90e09f8c5186ff1b876e5118bcf1f89c92
Geoff Low <glow@mdsol.com>
2022-09-13 11:28:09 +0100

    Add unscheduled visits

--- src/soa_bridge_match/dataset.py
+++ src/soa_bridge_match/dataset.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
-from datetime import date
+
 import datetime
 import pandas as pd
+import numpy as np
 import hashlib
 from math import floor
 import random
@@ -11,14 +12,23 @@ from typing import Optional
 from fhir.resources.bundle import Bundle
 from fhir.resources.careplan import CarePlan
 from fhir.resources.coding import Coding
+from fhir.resources.codeableconcept import CodeableConcept
+from fhir.resources.dosage import Dosage
+from fhir.resources.fhirtypes import (
+    DosageDoseAndRateType,
+    ObservationReferenceRangeType,
+)
 from fhir.resources.encounter import Encounter
 from fhir.resources.identifier import Identifier
 from fhir.resources.patient import Patient
 from fhir.resources.period import Period
 from fhir.resources.reference import Reference
+from fhir.resources.element import Element
+from fhir.resources.quantity import Quantity
 from fhir.resources.servicerequest import ServiceRequest
 from fhir.resources.medicationadministration import MedicationAdministration
 from fhir.resources.medicationrequest import MedicationRequest
+from fhir.resources.observation import Observation
 
 
 from .bundler import SourcedBundle
@@ -29,10 +39,21 @@ def hh(s: str) -> str:
     return hashlib.md5(s.encode("utf-8")).hexdigest()
 
 
+def to_datetime(date) -> datetime.datetime:
+    """
+    Converts a numpy datetime64 object to a python datetime object 
+    Input:
+      date - a np.datetime64 object
+    Output:
+      DATE - a python datetime object
+    """
+    timestamp = ((date - np.datetime64('1970-01-01T00:00:00'))
+                 / np.timedelta64(1, 's'))
+    return datetime.datetime.utcfromtimestamp(timestamp)
+
 class Naptha:
     def __init__(
-        self, templatefile: Optional[str], 
-        templatecontent: Optional[Bundle] = None
+        self, templatefile: Optional[str], templatecontent: Optional[Bundle] = None
     ) -> None:
         self._connector = Connector()
         self._subjects = {}
@@ -138,12 +159,13 @@ class Naptha:
         if subject_id not in self.get_subjects():
             raise ValueError(f"Subject {subject_id} does not exist")
         print("Adding medications for {}".format(subject_id))
-    
+
         patient_hash_id = hh(subject_id)
         # get the set of records for a subject
         ex = self.get_subject_ex(subject_id)
         dm = self.get_subject_dm(subject_id)
         sv = self.get_subject_sv(subject_id)
+        visit_1 = sv[sv["VISITNUM"] == 1.0].SVSTDTC.values[0]
         # get the arm
         arm = dm.ARMCD.unique()[0]
         # Medication Request
@@ -154,17 +176,77 @@ class Naptha:
             "Scrnfail": "",
         }
         records = []
+        # Add the TTS-Test
+        _tts_id = hh(f"{subject_id}-TTS-Test-Request")
+        tts_test_request = MedicationRequest(
+            id=_tts_id,
+            status="active",
+            medicationReference=Reference(
+                reference=f"Medication/H2Q-MC-LZZT-LY246708-Placebo-TTS"
+            ),
+            subject=Reference(reference=f"Patient/{patient_hash_id}"),
+            instantiatesCanonical=[f"ActivityDefinition/H2Q-MC-LZZT-Placebo-TTS-Admin"],
+            intent="order",
+            dosageInstruction=[
+                Dosage(
+                    route=CodeableConcept(
+                        coding=[Coding(code="45890007")], text="Transdermal route"
+                    ),
+                    doseAndRate=[
+                        DosageDoseAndRateType(
+                            type=CodeableConcept(text="ordered"),
+                            doseQuantity=Quantity(
+                                value=1,
+                                unit="TPATCH",
+                                system="http://terminology.hl7.org/CodeSystem/v3-orderableDrugForm",
+                            ),
+                        )
+                    ],
+                )
+            ],
+        )
+        self.content.add_resource(tts_test_request)
+        _tts_admin_id = hh(f"{subject_id}-TTS-Test-Request")
+        tts_admin = MedicationAdministration(
+            id=_tts_admin_id,
+            status="completed",
+            medicationReference=Reference(reference=f"Medication/{medication[arm]}"),
+            subject=Reference(reference=f"Patient/{patient_hash_id}"),
+            effectivePeriod=Period(
+                start=visit_1,
+                end=visit_1 + np.timedelta64(12, "h"),
+            ),
+            request=Reference(reference=f"MedicationRequest/{_tts_id}"),
+        )
+        self.content.add_resource(tts_admin)
         # get the medication request
         for offset, record in ex.iterrows():
             if pd.isna(record.EXENDTC):
                 print("Subject", subject_id, f"has missing end date at {offset}")
                 continue
+            # calculate the duration for this dosing period
             delta = (record.EXENDTC - record.EXSTDTC).days
-            print(f"Adding medication for {subject_id} from {record.EXSTDTC} to {record.EXENDTC} ({delta} days)")
-            if not isinstance(delta, (int, )):
-                print("Subject", subject_id, f"has a weird delta for {record.EXSEQ} Start {record.EXSTDTC} End {record.EXENDTC}")
-                delta = floor(delta)
+            # create a hash id for the medication request
             _mr_id = hh(f"{subject_id}-{record.EXSEQ}")
+            mr = MedicationRequest(
+                id=_mr_id,
+                intent="order",
+                status="active",
+                medicationReference=Reference(
+                    reference=f"Medication/{medication[arm]}"
+                ),
+                subject=Reference(reference=f"Patient/{patient_hash_id}"),
+            )
+            print(
+                f"Adding medication for {subject_id} from {record.EXSTDTC} to {record.EXENDTC} ({delta} days)"
+            )
+            if not isinstance(delta, (int,)):
+                print(
+                    "Subject",
+                    subject_id,
+                    f"has a weird delta for {record.EXSEQ} Start {record.EXSTDTC} End {record.EXENDTC}",
+                )
+                delta = floor(delta)
             # medication_request = MedicationRequest(id=_mr_id)
             for dt in range(delta):
                 _date = record.EXSTDTC + datetime.timedelta(days=dt)
@@ -172,11 +254,11 @@ class Naptha:
                 _stt = datetime.datetime.combine(_date, datetime.time(8, 0, 0))
                 # add a little variation around the start/end for the medication
                 if time.time() % 2 == 0:
-                    _start_time =  _stt - datetime.timedelta(
+                    _start_time = _stt - datetime.timedelta(
                         minutes=random.randint(0, 60)
                     )
                 else:
-                    _start_time =  _stt - + datetime.timedelta(
+                    _start_time = _stt - +datetime.timedelta(
                         minutes=random.randint(0, 60)
                     )
                 medication_admin = MedicationAdministration(
@@ -187,8 +269,8 @@ class Naptha:
                     ),
                     subject=Reference(reference=f"Patient/{patient_hash_id}"),
                     effectivePeriod=Period(
-                        start = _start_time,
-                        end = _start_time + datetime.timedelta(hours=12),
+                        start=_start_time,
+                        end=_start_time + datetime.timedelta(hours=12),
                     ),
                 )
                 # get the medication request
@@ -201,20 +283,21 @@ class Naptha:
             self.content.add_resource(record)
         print("Done")
 
-    def merge_unscheduled_visit(self, subject_id: str, visit_num: str):
+    def merge_unscheduled_visit(self, subject_id: str, visit_number: str):
         """
         Add an unscheduled visit for an individual
         """
         if subject_id not in self.get_subjects():
             raise ValueError(f"Subject {subject_id} does not exist")
         plan_def_id = "H2Q-MC-LZZT-Study-Unscheduled-Visit"
+        patient_hash_id = hh(subject_id)
         # slice the dataset
         sv = self.get_subject_sv(subject_id)
-        prior_visit = visit_number.split(".")[0]
-        before = sv[sv.VISITNUM == prior_visit]["SVSTDTC"].values[0]     
-        visdt = before + datetime.timedelta(days=random.randint(1, 7)))   
+        prior_visit = float(visit_number.split(".")[0])
+        before = to_datetime(sv[sv.VISITNUM == prior_visit]["SVSTDTC"].values[0])
+        visdt = before + datetime.timedelta(days=random.randint(1, 7))
         # generate the careplan
-        care_plan_description = f"{patient_hash_id}-{visit_num}-CarePlan"
+        care_plan_description = f"{patient_hash_id}-{visit_number}-CarePlan"
         care_plan_id = hh(care_plan_description)
         # create a care plan
         care_plan = CarePlan(
@@ -223,14 +306,12 @@ class Naptha:
             intent="order",
             subject=Reference(reference=f"Patient/{patient_hash_id}"),
             instantiatesCanonical=[f"PlanDefinition/{plan_def_id}"],
-            title=f"Subject {record.USUBJID} {visit_num}",
+            title=f"Subject {subject_id} {visit_number}",
         )
         care_plan.identifier = [Identifier(value=care_plan_description)]
         # bind the care plan - todo!
         # create the service request
-        service_request_description = (
-            f"{patient_hash_id}-{visit_num}-ServiceRequest"
-        )
+        service_request_description = f"{patient_hash_id}-{visit_number}-ServiceRequest"
         service_request_id = hh(service_request_description)
         service_request = ServiceRequest(
             id=service_request_id,
@@ -242,8 +323,9 @@ class Naptha:
         service_request.identifier = [
             Identifier(value=f"{service_request_description}")
         ]
+        _encounter_id = hh(f"{subject_id}-{visit_number}-Encounter")
         encounter = Encounter(
-            id=hh(f"{care_plan_id}-{record.VISITNUM}-Encounter"),
+            id=_encounter_id,
             status="finished",
             class_fhir=Coding(code="IMP", system="http://hl7.org/fhir/v3/ActCode"),
             subject=Reference(reference=f"Patient/{patient_hash_id}"),
@@ -260,7 +342,108 @@ class Naptha:
         self.content.add_resource(service_request)
         self.content.add_resource(encounter)
         # add the observations
-        
+        # EG
+        # VS
+        for vs_obs in (
+            ["8480-6", "Systolic blood pressure - standing", 110, 140, "mm[Hg]", 0],
+            ["8454-1", "Diastolic blood pressure - standing", 65, 90, "mm[Hg]", 0],
+            ["8867-4", "Body weight", 60, 100, "kg", 5],
+            ["8310-5", "Body Temperature", 36.2, 38.1, "kg", 10],
+            ["8867-4", "Heart Rate", 60, 100, "bpm", 0],
+        ):
+            # obs = self._generate_observation(
+            #     subject_id,
+            #     vs_obs[0],
+            #     vs_obs[1],
+            #     random.randint(vs_obs[2], vs_obs[3]),
+            #     vs_obs[4],
+            #     visdt,
+            # )
+            if isinstance(vs_obs[2], (int,)):
+                _value = random.randint(vs_obs[2], vs_obs[3])
+            else:
+                _value = random.uniform(vs_obs[2], vs_obs[3])
+
+            obs = Observation(
+                id=hh(f"{patient_hash_id}-{visit_number}-{vs_obs[0]}"),
+                status="final",
+                effectiveDateTime = datetime.datetime.combine(
+                    visdt, datetime.time(8, vs_obs[5], 0)
+                ),
+                code=CodeableConcept(
+                    coding=[
+                        Coding(
+                            code=vs_obs[0], display=vs_obs[1], system="http://loinc.org"
+                        )
+                    ],
+                    text=vs_obs[1],
+                ),
+                category=[
+                    CodeableConcept(
+                        coding=[
+                            Coding(
+                                code="vital-signs",
+                                display="Vital Signs",
+                                system="http://terminology.hl7.org/CodeSystem/observation-category",
+                            )
+                        ]
+                    )
+                ],
+                encounter=Reference(reference=f"Encounter/{_encounter_id}"),
+                subject=Reference(reference=f"Patient/{patient_hash_id}"),
+                valueQuantity=Quantity(value=_value, unit=vs_obs[4]),
+                basedOn=[Reference(reference=f"CarePlan/{care_plan_id}")],
+            )
+            self.content.add_resource(obs)
+        # LB
+        for lab_result in (
+            ["20570-8", "Hematocrit [Volume Fraction] of Blood", 34, 48, "%"],
+            ["2345-7", "Glucose [Mass/volume] in Serum or Plasma", 50, 250, "mg/dL"],
+            ["718-7", "Hemoglobin [Mass/volume] in Blood", 11.5, 15.8, "g/dl"],
+            [
+                "2823-3",
+                "Potassium [Moles/volume] in Serum or Plasma",
+                3.4,
+                5.4,
+                "mEq/L",
+            ],
+        ):
+            if isinstance(lab_result[2], (int,)):
+                _value = random.randint(lab_result[2], lab_result[3])
+            else:
+                _value = random.uniform(lab_result[2], lab_result[3])
+            _lb = Observation(
+                id=hh(f"{patient_hash_id}-{visit_number}-{lab_result[0]}"),
+                status="final",
+                effectiveDateTime=datetime.datetime.combine(visdt, datetime.time(8, 15, 0)),
+                code=CodeableConcept(
+                    coding=[Coding(code=lab_result[0])], text=lab_result[1]
+                ),
+                category=[
+                    CodeableConcept(
+                        coding=[
+                            Coding(
+                                code="laboratory",
+                                display="Laboratory",
+                                system="http://terminology.hl7.org/CodeSystem/observation-category",
+                            )
+                        ]
+                    )
+                ],
+                subject=Reference(reference=f"Patient/{patient_hash_id}"),
+                referenceRange=[
+                    ObservationReferenceRangeType(
+                        low=Quantity(value=lab_result[2], unit=lab_result[4]),
+                        high=Quantity(value=lab_result[3], unit=lab_result[4]),
+                    )
+                ],
+                valueQuantity=Quantity(
+                    value=_value,
+                    unit=lab_result[4],
+                ),
+                encounter=Reference(reference=f"Encounter/{_encounter_id}"),
+                basedOn=[Reference(reference=f"CarePlan/{care_plan_id}")],
+            )
 
     def merge_sv(self, subject_id: Optional[str] = None):
         """
